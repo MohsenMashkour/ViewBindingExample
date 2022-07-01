f1674b6aaae432606d02fd98032f77b8d1fa4b3f
Mohsen Mashkour <49885171+MohsenMashkour@users.noreply.github.com>
2022-07-01 12:14:13 +0200

    first

---
+++ app/src/main/java/com/mkrdeveloper/viewbindingexample/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.mkrdeveloper.viewbindingexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.mkrdeveloper.viewbindingexample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.tvHello.text = "mkr developer"
+
+
+        binding.btnTest.setOnClickListener {
+
+        }
+
+    }
+}
